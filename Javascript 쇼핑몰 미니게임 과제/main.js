b0d66a4867bfeaa2d4cff305ebc027256bab5952
l-wanderer01 <jhjjang0423@naver.com>
2025-01-01 22:07:04 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -1,15 +1,49 @@
-// JSON file로부터 데이터를 받아와서, 데이터를 화면에 렌더링하는 기능을 구현
+// Fetch the items from the JSON file
 function loadItems() {
-    return fetch('data/data.json')
+    return fetch('./data/data.json')
         .then(response => response.json())
         .then(json => json.items);
+}
+
+// Update the list with the given items
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+            <span class="item__discription">${item.gender}, ${item.size}</span>
+        </li>
+    `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key == null || value == null) {
+        return;
+    }
+
+    displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
 }
 
 // main
 loadItems()
     .then(items => {
-        console.log(items);
-        // displayItems(items);
+        displayItems(items);
         // setEventListeners(items);
     })
     .catch(console.log);
