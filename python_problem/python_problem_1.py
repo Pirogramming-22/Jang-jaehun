30366d42d5b71504b0529fdb9893e0c9a01b723f
l-wanderer01 <jhjjang0423@naver.com>
2025-01-02 22:09:58 +0900

    5단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -4,17 +4,53 @@ num = 0
 # TODO 3-1: 2단계에서 정수를 입력하지 않는 경우, `정수를 입력하세요`를 출력한다.
 # TODO 3-2: 2단계에서 1,2,3을 입력하지 않는 경우, `1,2,3 중 하나를 입력하세요`를 출력한다.
 # TODO 3-3: 올바른 값이 입력될 때까지 입력을 요구한다.
-while True:
-    try:
-        # TODO 2: input() 함수를 이용하여 1에서 3사이의 정수를 입력받는 코드를 작성하여라.
-        user = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-    except ValueError:
-        print("정수를 입력하세요")
-    else:
-        if not (1 <= user <= 3):
-            print("1, 2, 3 중 하나를 입력하세요")
+def user_input(num):
+    while True:
+        try:
+            # TODO 2: input() 함수를 이용하여 1에서 3사이의 정수를 입력받는 코드를 작성하여라.
+            user = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+        except ValueError:
+            print("정수를 입력하세요")
         else:
-            break
+            if not (1 <= user <= 3):
+                print("1, 2, 3 중 하나를 입력하세요")
+            else:
+                break
+    return user
 
-for i in range(1, user + 1):
-    print(f"player : {i}")
+# TODO 4: 변수 num 을 이용하여, 2단계에서 입력한 수만큼 숫자를 출력하는 코드를 작성하여라.
+""" 
+TODO 5:
+- 1에서 3사이의 정수를 입력받는 코드를 작성하여라.
+    
+     `부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :`
+    
+- 정수를 입력하지 않는 경우, `정수를 입력하세요`를 출력한다.
+- 1,2,3을 입력하지 않는 경우, `1,2,3 중 하나를 입력하세요`를 출력한다.
+- 올바른 값이 입력될 때까지 입력을 요구한다.
+- 변수 `num`을 이용하여 입력한 수만큼 숫자를 출력하는 코드를 작성하여라.
+"""
+
+def check_num(num, user, player):
+    for i in range(num, num + user): # user가 입력한 수만큼 반봅
+        print(f"player{player} : {i + 1}")
+    # user switch
+    if (player == "A"):
+        player = "B"
+    else:
+        player = "A"
+    # num 값 업데이트
+    num += user
+    ### 디버깅 코드
+    print(num)
+    return num, player
+
+# default user
+player = "A"
+
+# main code
+while True:
+    user = user_input(num)
+    num, player = check_num(num, user, player)
+    ### 디버깅 코드
+    print(num)
