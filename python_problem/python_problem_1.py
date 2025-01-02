57f429e3480a03c45434050e835cfa1a8d26cda3
l-wanderer01 <jhjjang0423@naver.com>
2025-01-03 02:08:27 +0900

    9단계 완료

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,6 +1,18 @@
+'''
+TODO 9:
+- 위의 코드에서 playerA와 playerB는 각각 computer와 player로 변경한다.
+- computer는 임의로 1~3개의 수를 부른다.
+- 참고) 파이썬에서 난수(random number)를 생성하기 위해 `random` 모듈을 사용한다.
+    - `random` 모듈의 `randint(a, b)` 함수는 a부터 b사이(끝값 포함)의 임의의 정수를 리턴한다.
+    - 예를 들어, `randint(1, 100)`은 1부터 100사이(1과 100포함)의 임의의 정수를 리턴한다.
+'''
+
+# 난수 생성을 위해 random 모듈을 import
+import random
+
 # TODO 1: 변수 num을 0으로 선언
 num = 0
-player = "A"
+flag = random.randint(0,1) # computer인지 user인지 확인하기 위한 flag 0: computer, 1: user
 
 # TODO 3-1: 2단계에서 정수를 입력하지 않는 경우, `정수를 입력하세요`를 출력한다.
 # TODO 3-2: 2단계에서 1,2,3을 입력하지 않는 경우, `1,2,3 중 하나를 입력하세요`를 출력한다.
@@ -34,47 +46,52 @@ TODO 5:
 
 # TODO 8: 6단계까지 중복되는 코드를 찾아 함수로 만들어라. 이때, 함수 이름은 brGame으로 한다.
 def brGame():
-    global player
-    if (player == "A"):
-        player = "B"
-    else:
-        player = "A"
+    global flag
 
-def player_print(player, i):
-    print(f"player{player} : {i + 1}")
+    if (flag == 0):
+        flag = 1
+    else:
+        flag = 0
+
+def num_print(flag, i):
+    if flag == 0:
+        print(f"computer : {i + 1}")
+    else:
+        print(f"player : {i + 1}")
 
 # TODO 6: 배스킨라빈스31 게임은 참여자가 번갈아가며 숫자를 부른다. 게임이 끝날 때까지 playerA와 playerB에게 번갈아가며 부를 숫자의 개수를 입력받는 코드를 작성하여라.
-def check_num(num, user, player):
-    for i in range(num, num + user): # user가 입력한 수만큼 반복
+def check_num(num, input, flag):
+    for i in range(num, num + input): # user가 입력한 수만큼 반복
         # TODO 6-1: 값에 31이 포함되어있는지 확인(포함되어있다면 해당 플레이어와 num 리턴)
         if i == 30:
-            player_print(player, i)
-            num += user
+            num_print(flag, i)
+            num += input
             return num
-        player_print(player, i)
-    # if (player == "A"):
-    #     player = "B"
-    # else:
-    #     player = "A"
+        num_print(flag, i)
     brGame()
     # num 값 업데이트
-    num += user
+    num += input
     return num
 
 # main code
 while True:
+    input_num = 0
     # default user
-    user = user_input(num)
+    if flag == 0:
+        computer = random.randint(1, 3)
+        input_num = computer
+    else:
+        user = user_input(num)
+        input_num = user
     # num, player = check_num(num, user, player)
-    num = check_num(num, user, player)
+    num = check_num(num, input_num, flag)
     # ### 디버깅 코드
     # print(f"final_num : {num}")
     if num >= 31:
-        # TODO 7: 게임이 끝났을 때, 누가 이겼는지 화면에 출력하여라.
-        # if (player == "A"):
-        #     player = "B"
-        # else:
-        #     player = "A"
         brGame()
-        print(f"player{player} win")
+        # TODO 7: 게임이 끝났을 때, 누가 이겼는지 화면에 출력하여라.
+        if flag == 0:
+            print("computer win!")
+        else:
+            print("player win!")
         break
